92ab48b88b1d26766f3d764b48f7c0fb99734271
J3RSS0N17 <jersson1108@gmail.com>
2026-02-27 06:46:34 -0500

    Update NotificacionService.java

--- NotificacionService.java
+++ NotificacionService.java
@@ -1,9 +1,1 @@
-public class NotificacionService{private final NotificacionRepository repository; 
-                                 public NotificacionService(NotificacionRepository reposiory){this.repository=reposiory;}}
-public Notificacion crear (Notificacion notificacion){return repository.save(notificacion);}
-public List<Notificacion> listarPorUsuario(Long usuarioid){return repository.findByUsuarioId(usuarioId);}
-public void marcarComoLeida(Long id){Notificacion notificacion=repository-findById(id).orElseThrow(()->RuntimeException("notificacion no encontrada"));
-    notificacion.setLeida(true);
-    repository.save(notificacion);
-}
-public void eliminar(Long id){repository.deletById(id);}
+
