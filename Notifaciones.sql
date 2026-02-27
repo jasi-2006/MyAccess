29b77c5fc4a1160da2a32ef058f33662d354477b
J3RSS0N17 <jersson1108@gmail.com>
2026-02-27 06:46:59 -0500

    Delete Notifaciones.sql

--- Notifaciones.sql
+++
@@ -1,7 +0,0 @@
-create table novedades(
-    id_notificacion int primary key auto_increment, 
-    FOREIGN KEY (id_novedad) REFERENCES novedad(id_novedad) 
-    fecha_notificacion datetime, 
-    FOREIGN KEY (id_user) REFERENCES user(id_user) 
-
-)
