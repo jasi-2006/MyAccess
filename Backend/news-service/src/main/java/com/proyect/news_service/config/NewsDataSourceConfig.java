a8907aed40b5b646b3fd9575ef356614936f7d71
paulo <paulorodriguez733@gmail.com>
2026-05-23 15:41:35 -0500

    se corrige configuracion de el servicio de news

--- Backend/news-service/src/main/java/com/proyect/news_service/config/NewsDataSourceConfig.java
+++ Backend/news-service/src/main/java/com/proyect/news_service/config/NewsDataSourceConfig.java
@@ -48,7 +48,6 @@ public class NewsDataSourceConfig {
         factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         Properties props = new Properties();
         props.setProperty("hibernate.hbm2ddl.auto", "update");
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         factory.setJpaProperties(props);
         return factory;
     }
