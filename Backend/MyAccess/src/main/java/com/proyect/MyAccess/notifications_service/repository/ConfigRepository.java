0deb8c1191dbabd48cead7899cdb00371b5d1911
paulo <paulorodriguez733@gmail.com>
2026-05-23 12:30:59 -0500

    se elimina el monolito original

--- Backend/MyAccess/src/main/java/com/proyect/MyAccess/notifications_service/repository/ConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyect.MyAccess.notifications_service.repository;
-
-import com.proyect.MyAccess.notifications_service.entity.Config;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ConfigRepository extends JpaRepository<Config, Long> {
-    Optional<Config> findByClue(String clue);
-}
--- Backend/MyAccess/src/main/java/com/proyect/MyAccess/user_service/config/CorsConfig.java
+++
@@ -1,5 +0,0 @@
-package com.proyect.MyAccess.user_service.config;
-
-// CORS ahora se maneja en FilterConfig con orden HIGHEST_PRECEDENCE
-public class CorsConfig {
-}
