535ba1563c74fe3d1f0767ba5f5a375cf35715bb
paulo <paulorodriguez733@gmail.com>
2026-05-26 19:41:54 -0500

    se realiza correccion en el kong para el deslpirgue

--- Backend/kong-gateway/start-kong.sh
+++ Backend/kong-gateway/start-kong.sh
@@ -7,6 +7,9 @@ if [ -z "${JWT_SECRET:-}" ]; then
 fi
 
 export JWT_SECRET
-envsubst '${JWT_SECRET}' < /kong/declarative/kong.yml.template > /kong/declarative/kong.yml
+# /kong/declarative is not writable as USER kong; use /tmp for generated config.
+CONFIG_OUT="${KONG_DECLARATIVE_CONFIG:-/tmp/kong-declarative.yml}"
+envsubst '${JWT_SECRET}' < /kong/declarative/kong.yml.template > "$CONFIG_OUT"
+export KONG_DECLARATIVE_CONFIG="$CONFIG_OUT"
 
 exec /docker-entrypoint.sh kong docker-start
