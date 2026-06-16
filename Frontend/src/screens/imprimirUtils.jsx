2c5fb757a702374efc26bee0010ea5232c7ad94f
paulo <paulorodriguez733@gmail.com>
2026-06-16 14:42:48 -0500

    se ajustan tamaños de la imprecion de carnert

--- Frontend/src/screens/imprimirUtils.jsx
+++ Frontend/src/screens/imprimirUtils.jsx
@@ -172,7 +172,7 @@ export function buildCarnetPairHtml(learner, card) {
         <div style="width:88px;display:flex;align-items:center;">
           ${logoHtml}
         </div>
-        <div style="width:202px;height:202px;overflow:hidden;background:#E9E9E9;">
+        <div style="width:190px;height:190px;overflow:hidden;background:#E9E9E9;">
           ${photoHtml}
         </div>
       </div>
